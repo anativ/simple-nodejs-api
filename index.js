4753f51ce0297f08ea353b55c41f6f83b27bc5ff
anativ <alon.nativ@tomorrow.io>
2024-02-10 12:45:12 +0200

    log port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3001; // Heroku will set the PORT environment variable
-
+console.log('port', port);
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
