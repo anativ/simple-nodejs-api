46ddaf4cfdb5be38886b895946beeff8bd7c0459
anativ <alon.nativ@tomorrow.io>
2024-02-10 12:49:31 +0200

    remove host

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3001; // Heroku will set the PORT environment variable
 console.log('port', port);
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
